b66ad4db5a7e6427d844daf46736a836eadd6824
castroneves <adam.heinke@gmail.com>
2017-06-03 19:34:25 +0100

    Only retry on 429

--- src/main/java/spotify/domain/BlockingResponse.java
+++ src/main/java/spotify/domain/BlockingResponse.java
@@ -5,20 +5,26 @@ import java.util.Optional;
 /**
  * Created by castroneves on 28/11/2016.
  */
-public class BlockingResponse<T> {
-    private Optional<T> response;
+public class BlockingResponse<T,S> {
+    private Optional<S> response;
+    private int status;
     private FuncTuple<T> func;
 
-    public BlockingResponse(Optional<T> response, FuncTuple<T> func) {
+    public BlockingResponse(Optional<S> response,int status, FuncTuple<T> func) {
         this.response = response;
+        this.status = status;
         this.func = func;
     }
 
-    public Optional<T> getResponse() {
+    public Optional<S> getResponse() {
         return response;
     }
 
     public FuncTuple<T> getFunc() {
         return func;
+    }
+
+    public int getStatus() {
+        return status;
     }
 }
